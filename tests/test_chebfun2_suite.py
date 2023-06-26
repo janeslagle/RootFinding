99f29b7e3909b52ded6e2548364d97fe221df63f
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2023-06-26 15:46:48 -0600

    Update test_chebfun2_suite.py

--- tests/test_chebfun2_suite.py
+++ tests/test_chebfun2_suite.py
@@ -260,11 +260,11 @@ def verbose_pass_or_fail(funcs, yroots, polished_roots, test_num, cheb_roots=Non
             else:
                 print("\t YRoots norm test: fail")
             print("The norm of the difference in x values:", x_norm)
-            print("The norm of the difference in y values:", y_norm)
+            print("The norm of the difference in y values:", y_norm)"""
         except ValueError as e:
                 print("A different number of roots were found.")
                 print ("Yroots: " + str(len(yroots)))
-                print("Polished: " + str(len(polished_roots)))"""
+                print("Polished: " + str(len(polished_roots)))
     print("YRoots max residuals:")
     YR_resid = list()
     for i, func in enumerate(funcs):

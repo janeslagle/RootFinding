75f3f716b758cd5be9310ec742971220df623086
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2023-06-27 12:08:33 -0600

    Update test_chebfun2_suite.py

--- tests/test_chebfun2_suite.py
+++ tests/test_chebfun2_suite.py
@@ -636,9 +636,9 @@ if __name__ == "__main__":
     print(f'Residual Test: Passed {np.sum(res_passes)} of 20, {100*np.mean(res_passes)}%')
     where_failed_res = np.where(~res_passes)[0]
     failed_res_tests = tests[where_failed_res]
-    #if (len(failed_res_tests) != 0):
-    #	print(f'Failed Residual Test on \n{[t.__name__ for t in failed_res_tests]}')
-    assert len(failed_res_tests) != 0, print(f'Failed Residual Test on \n{[t.__name__ for t in failed_res_tests]}')    
+    if (len(failed_res_tests) != 0):
+    	print(f'Failed Residual Test on \n{[t.__name__ for t in failed_res_tests]}')
+    assert len(failed_res_tests) > 0, "FAILED"    
 
     print(f'Norm Test    : Passed {np.sum(norm_passes)} of 20, {100*np.mean(norm_passes)}%')
     where_failed_norm = np.where(~norm_passes)[0]

e4530ddd27e19682765486f2ff5996db26d3d517
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2023-07-07 10:47:59 -0700

    Update test_Combined_Solver.py

--- tests/test_Combined_Solver.py
+++ tests/test_Combined_Solver.py
@@ -89,16 +89,16 @@ def test_bad_intervals():
     (a) a lower bound is greater than an upper bound
     (b) the bounding arrays are unequal in length
     """
-    a,b = np.array([1,-1]),np.array([1,1])
-    funcs = [f,g]
-    with pytest.raises(ValueError) as excinfo:
-        solve([f,g],a,b,[f_deg,g_deg])
-    assert excinfo.value.args[0] == "At least one lower bound is >= an upper bound."
-
-    #a = [a[0]]
+    #a,b = np.array([1,-1]),np.array([1,1])
+    #funcs = [f,g]
     #with pytest.raises(ValueError) as excinfo:
     #    solve([f,g],a,b,[f_deg,g_deg])
-    #assert excinfo.value.args[0] == "Dimension mismatch in intervals."
+    #assert excinfo.value.args[0] == "At least one lower bound is >= an upper bound."
+
+    a = [a[0]]
+    with pytest.raises(ValueError) as excinfo:
+        solve([f,g],a,b,[f_deg,g_deg])
+    assert excinfo.value.args[0] == "Dimension mismatch in intervals."
     
     return True
 

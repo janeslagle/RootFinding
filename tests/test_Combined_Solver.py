0dce88ac870424fcb755c930e65e5dedb4ecd88c
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2023-07-07 10:04:36 -0700

    Update test_Combined_Solver.py

--- tests/test_Combined_Solver.py
+++ tests/test_Combined_Solver.py
@@ -248,11 +248,11 @@ if __name__ == '__main__':
     if (test_bad_intervals()):
         tests_passed += 1
     if (test_exact_option()):
-        tests_passed += 1"""
+        tests_passed += 1
     if (testreturnBoundingBoxes):
         tests_passed += 1"""
     if (testoutside_neg1_pos1()):
-        tests_passed += 1
+        tests_passed += 1"""
     if (test_default_nodeg()):
         tests_passed += 1
     if (test_deg_inf()):

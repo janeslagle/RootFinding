9662cc604c59a00611ae3758cc63f73ef4c12826
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2023-08-27 21:25:50 -0700

    Update polynomial.py

--- yroots/polynomial.py
+++ yroots/polynomial.py
@@ -29,7 +29,7 @@ class Term(object):
 
     def __ge__(self, other):
         return (self > other or self == other)
-​
+        
     def __le__(self,other):
         return (self < other or self == other)
 ​

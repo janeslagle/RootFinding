3dc007b200475dc7853e24f7673303874c3a6dd3
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2023-10-08 23:17:34 -0600

    Update Combined_Solver.py

--- yroots/Combined_Solver.py
+++ yroots/Combined_Solver.py
@@ -1,33 +1,35 @@
 import numpy as np
 from numba import njit
+import itertools
 import yroots.ChebyshevSubdivisionSolver as ChebyshevSubdivisionSolver
 import yroots.ChebyshevApproximator as ChebyshevApproximator
 from yroots.polynomial import MultiCheb
 
-def solve(funcs,a=-1,b=1, verbose = False, returnBoundingBoxes = False, exact=False):
+def solve(funcs,a=-1,b=1, verbose = False, returnBoundingBoxes = False, exact=False, minBoundingIntervalSize=1e-5):
+
     """Finds and returns the roots of a system of functions on the search interval [a,b].
-​
+
     Generates an approximation for each function using Chebyshev polynomials on the interval given,
     then uses properties of the approximations to shrink the search interval. When the information
     contained in the approximation is insufficient to shrink the interval further, the interval is
     subdivided into subregions, and the searching function is recursively called until it zeros in
     on each root. A specific point (and, optionally, a bounding box) is returned for each root found.
-​
+
     NOTE: YRoots uses just in time compiling, which means that part of the code will not be compiled until
     a system of functions to solve is given (rather than compiling all the code upon importing the module).
     As a result, the very first time the solver is given any system of equations of a particular dimension,
     the module will take several seconds longer to solve due to compiling time. Once the first system of a
     particular dimension has run, however, other systems of that dimension (or even the same system run
     again) will be solved at the normal (faster) speed thereafter.
-​
+
     NOTE: The solve function is only guaranteed to work well on systems of equations where each function
     is continuous and smooth and each root in the interval is a simple root. If a function is not
     continuous and smooth on an interval or an infinite number of roots exist in the interval, the
     solver may get stuck in recursion or the kernel may crash.
-​
+
     Examples
     --------
-​
+
     >>> f = lambda x,y,z: 2*x**2 / (x**4-4) - 2*x**2 + .5
     >>> g = lambda x,y,z: 2*x**2*y / (y**2+4) - 2*y + 2*x*z
     >>> h = lambda x,y,z: 2*z / (z**2-4) - 2*z
@@ -36,15 +38,15 @@ def solve(funcs,a=-1,b=1, verbose = False, returnBoundingBoxes = False, exact=Fa
     [[-4.46764373e-01  4.44089210e-16 -5.55111512e-17]
      [ 4.46764373e-01  4.44089210e-16 -5.55111512e-17]]
     
-​
-​
+
+
     >>> M1 = yroots.MultiPower(np.array([[0,3,0,2],[1.5,0,7,0],[0,0,4,-2],[0,0,0,1]]))
     >>> M2 = yroots.MultiCheb(np.array([[0.02,0.31],[-0.43,0.19],[0.06,0]]))
     >>> roots = yroots.solve([M1,M2],-5,5)
     >>> print(roots)
     [[-0.98956615 -4.12372817]
      [-0.06810064  0.03420242]]
-​
+
     Parameters
     ----------
     funcs: list
@@ -67,7 +69,12 @@ def solve(funcs,a=-1,b=1, verbose = False, returnBoundingBoxes = False, exact=Fa
     exact: bool
         Defaults to False. Whether transformations performed on the approximation should be performed
         with higher precision to minimize error.
-​
+    minBoundingIntervalSize : double
+        Defaults to 1e-5. If a root in found with a bounding interval of size > minBoundingIntervalSize in
+        each dimension, the functions are solved again on the smaller interval. Setting too small could cause
+        issues if the functions can't be evaluated accurately on points close together, and will increase solve
+        times. Should give more accurate roots when smaller.
+
     Returns
     -------
     yroots : numpy array
@@ -95,40 +102,81 @@ def solve(funcs,a=-1,b=1, verbose = False, returnBoundingBoxes = False, exact=Fa
         raise ValueError(f"Invalid input: {len(a)} lower bounds were given but {len(b)} upper bounds were given")
     if (b<a).any():
         raise ValueError(f"Invalid input: at least one lower bound is greater than the corresponding upper bound.")
-    funcs = np.array(funcs)
+    polys = np.array(funcs)
     errs = np.array([0.]*dim)
     
-    # Check to see if the bounds are [-1,1]^n (if so, no final transformation will be needed)
-    is_neg1_1 = True
-    arr_neg1 = -np.ones(dim)
-    arr_1 = np.ones(dim)
-    if not np.allclose(arr_neg1,a,rtol=1e-08) or not np.allclose(arr_1,b,rtol=1e-08):
-        is_neg1_1 = False
-
     # Get an approximation for each function.
     if verbose:
         print("Approximation shapes:", end=" ")
     for i in range(dim):
-        funcs[i], errs[i] = ChebyshevApproximator.chebApproximate(funcs[i],a,b)
+        polys[i], errs[i] = ChebyshevApproximator.chebApproximate(funcs[i],a,b)
         if verbose:
-            print(f"{i}: {funcs[i].shape}", end = " " if i != dim-1 else '\n')
+            print(f"{i}: {polys[i].shape}", end = " " if i != dim-1 else '\n')
     if verbose:
-        print(f"Searching on interval {[[a[i], b[i]] for i in range(dim)]}")
-
+        print(f"Searching on interval {[[a[i],b[i]] for i in range(dim)]}")
+        
+    #Solve the Chebyshev polynomial system
+    yroots, boundingBoxes = ChebyshevSubdivisionSolver.solveChebyshevSubdivision(polys,errs,verbose,True,exact,
+                constant_check=True, low_dim_quadratic_check=True, all_dim_quadratic_check=False)
+    
+    #If the bounding box is the entire interval, subdivide it!
+    usingSubdivision = np.all(b-a > minBoundingIntervalSize)
+    if len(boundingBoxes) == 1 and np.all(boundingBoxes[0].finalDimSize() == 2) and usingSubdivision:
+        #Subdivide the interval and resolve to get better resolution across different parts of the interval
+        yroots, boundingBoxes = [], []
+        for val in itertools.product([False, True], repeat=len(a)):
+            #Split almost in half
+            #TODO: Do we need to combine bounding boxes in this step of the recursion as well?
+            #      For now it seems safe enough to assume we won't have any roots on the midpoints.
+            midPoint = (a + b) * 0.51234912839471234
+            newA = np.where(val, midPoint, a)
+            newB = np.where(val, b, midPoint)
+            #Solve recursively
+            if verbose:
+                print("Re-solving on:", newA, newB)
+            roots, boxes = solve(funcs, a=newA, b=newB, verbose=verbose, returnBoundingBoxes=True, exact=exact, minBoundingIntervalSize = minBoundingIntervalSize)
+            if len(roots) != 0:
+                boundingBoxes.append(boxes)
+                yroots.append(roots)
+        if len(yroots) > 0:
+            yroots = np.vstack(yroots)
+            boundingBoxes = np.vstack(boundingBoxes)
+        if returnBoundingBoxes:
+            return yroots, boundingBoxes
+        else:
+            return yroots
+    
+    #TODO: Handle if we have duplicate roots or extra roots at the top level. Easiest if we actually return the bounding boxes!
+    #Maybe return the bounding boxes in the recursive steps?
+    
+    #If any of the bounding boxes is too large, re-solve that box.
+    finalBoxes = []
+    finalRoots = []
+    for box in boundingBoxes:
+        if np.all(box.finalDimSize() > minBoundingIntervalSize):
+            #Re-solve this box
+            newA, newB = ChebyshevApproximator.transform(box.finalInterval.T,a,b)
+            if verbose:
+                print("Re-solving on:", newA, newB)
+            roots, boxes = solve(funcs, a=newA, b=newB, verbose=verbose, returnBoundingBoxes=True, exact=exact, minBoundingIntervalSize = minBoundingIntervalSize)
+            if len(roots) > 0:
+                finalRoots.append(roots)
+                finalBoxes.append(boxes)
+        else:
+            #Transform back
+            finalBoxes.append([ChebyshevApproximator.transform(box.finalInterval.T,a,b).T])
+            #Get the roots from this box
+            if len(box.possibleDuplicateRoots) > 0:
+                finalRoots.append(ChebyshevApproximator.transform(np.array(box.possibleDuplicateRoots),a,b))
+            else:
+                finalRoots.append(ChebyshevApproximator.transform(box.getFinalPoint(),a,b))
+    if len(finalBoxes) != 0:
+        finalBoxes = np.vstack(finalBoxes)
+    if len(finalRoots) != 0:
+        finalRoots = np.vstack(finalRoots)
+    
     # Find and return the roots (and, optionally, the bounding boxes)
     if returnBoundingBoxes:
-        yroots, boundingBoxes = ChebyshevSubdivisionSolver.solveChebyshevSubdivision(funcs,errs,verbose,returnBoundingBoxes,exact,
-                constant_check=True, low_dim_quadratic_check=True,
-                all_dim_quadratic_check=False)
-        boundingBoxes = np.array([boundingBox.interval for boundingBox in boundingBoxes])
-        if is_neg1_1 == False and len(yroots) > 0: 
-            yroots = ChebyshevApproximator.transform(yroots,a,b)
-            boundingBoxes = np.array([ChebyshevApproximator.transform(boundingBox.T,a,b).T for boundingBox in boundingBoxes]) #xx yy, roots are xy xy each row
-        return yroots, boundingBoxes
+        return finalRoots, finalBoxes
     else:
-        yroots = ChebyshevSubdivisionSolver.solveChebyshevSubdivision(funcs,errs,verbose,returnBoundingBoxes,exact,
-                constant_check=True, low_dim_quadratic_check=True,
-                all_dim_quadratic_check=False)
-        if is_neg1_1 == False and len(yroots) > 0:
-            yroots = ChebyshevApproximator.transform(yroots,a,b)
-        return yroots
+        return finalRoots
